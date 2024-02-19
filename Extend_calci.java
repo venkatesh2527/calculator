90c3641d28728a166e36a17c9fccf888ba7d984d
venkatesh2527 <01fe22bca006@kletech.ac.in>
2024-02-19 15:23:39 +0530

    code on calci

---
+++ Extend_calci.java
@@ -0,0 +1,10 @@
+public class Extend_calci extends calculator {
+    void square(int a) {
+        System.out.println("Square of number is : " + (a * a));
+    }
+
+    void cube(int a) {
+        System.out.println("Cube of number is : " + (a * a * a));
+    }
+
+}
