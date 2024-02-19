caf58f2ab575de6eb1d1a4fad30e705f65a1b2ea
venkatesh2527 <01fe22bca006@kletech.ac.in>
2024-02-19 15:18:27 +0530

    code on calculator

---
+++ calculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    void calci() {
+        double num1 = 10, num2 = 20;
+
+        System.out.println("Num1=" + num1);
+        System.out.println("Num2=" + num2);
+
+        System.out.println("Addition=" + (num1 + num2));
+        System.out.println("Substraction=" + (num1 - num2));
+        System.out.println("Multiplication=" + (num1 * num2));
+        System.out.println("Division=" + (num1 / num2));
+    }
+}
+
+class arithmatic {
+    public static void main(String[] args) {
+        calculator z = new calculator();
+        z.calci();
+        
+    }
+}
